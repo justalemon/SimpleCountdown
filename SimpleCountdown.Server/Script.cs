9035a52e7d5740c97a8332c44a43caa1865d74f3
Hannele Ruiz <justlemoncl@gmail.com>
2020-05-28 02:03:58 -0400

    Added command to create a countdown

--- SimpleCountdown.Server/Script.cs
+++ SimpleCountdown.Server/Script.cs
@@ -1,4 +1,5 @@
 ﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using System.Collections.Generic;
 
 namespace SimpleCountdown.Server
@@ -14,6 +15,36 @@ namespace SimpleCountdown.Server
         /// Players and their countdowns created or joined.
         /// </summary>
         private readonly Dictionary<Player, Countdown> countdowns = new Dictionary<Player, Countdown>();
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Command that creates a countdown.
+        /// </summary>
+        [Command("ccreate")]
+        public void CommandCreate(int source, List<object> param, string raw)
+        {
+            // If the user is not allowed to create a countdown, ignore it silently
+            if (!API.IsPlayerAceAllowed(source.ToString(), "simplecountdown.create"))
+            {
+                return;
+            }
+
+            // Make a player object
+            Player player = Players[source];
+
+            // If the player has a countdown already, notify him
+            if (countdowns.ContainsKey(player))
+            {
+                player.TriggerEvent("simplecountdown:notify", $"You can't create a countdown when you are already in one.");
+                return;
+            }
+
+            // Otherwise, create a new countdown for the player
+            countdowns[player] = new Countdown(player);
+        }
 
         #endregion
     }
