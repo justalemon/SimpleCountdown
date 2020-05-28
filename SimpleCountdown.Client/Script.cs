2c64d40195eff010ccdc1ee198fc4676557e2893
Hannele Ruiz <justlemoncl@gmail.com>
2020-05-28 00:10:57 -0400

    Added scaleform loading for the COUNTDOWN

--- SimpleCountdown.Client/Script.cs
+++ SimpleCountdown.Client/Script.cs
@@ -1,4 +1,6 @@
 ﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System.Threading.Tasks;
 
 namespace SimpleCountdown.Client
 {
@@ -7,5 +9,42 @@ namespace SimpleCountdown.Client
     /// </summary>
     public class Script : BaseScript
     {
+        #region Fields
+
+        /// <summary>
+        /// The ID of the scaleform used to show the Countdown.
+        /// </summary>
+        private static int scaleform = 0;
+
+        #endregion
+
+        #region Constructor
+
+        public Script()
+        {
+            LoadScaleform();
+        }
+
+        #endregion
+
+        #region Tools
+
+        /// <summary>
+        /// Loads and saves the countdown scaleform.
+        /// </summary>
+        public async Task LoadScaleform()
+        {
+            // Request the Scaleform
+            int s = API.RequestScaleformMovie("COUNTDOWN");
+            // Wait until it has been loaded
+            while (API.HasScaleformMovieLoaded(s))
+            {
+                await Delay(0);
+            }
+            // And save it for later use
+            scaleform = s;
+        }
+
+        #endregion
     }
 }
