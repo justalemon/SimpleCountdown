2d3489d14e0655c0b65140258a668edf29751ef1
Hannele Ruiz <justlemoncl@gmail.com>
2020-05-28 00:09:14 -0400

    Added basic game scripts

---
+++ SimpleCountdown.Client/Script.cs
@@ -0,0 +1,11 @@
+﻿using CitizenFX.Core;
+
+namespace SimpleCountdown.Client
+{
+    /// <summary>
+    /// Client script that shows a countdown to the user. 
+    /// </summary>
+    public class Script : BaseScript
+    {
+    }
+}
---
+++ SimpleCountdown.Server/Script.cs
@@ -0,0 +1,11 @@
+﻿using CitizenFX.Core;
+
+namespace SimpleCountdown.Server
+{
+    /// <summary>
+    /// Server Script that handles the countdown.
+    /// </summary>
+    public class Script : BaseScript
+    {
+    }
+}
